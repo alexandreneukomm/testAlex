f083b32fb4eb39dce0b1571f4ba02b623b196d82
Alex Neukomm <alexandre.neukomm@he-arc.ch>
2018-10-08 20:31:58 +0200

    liste constante

--- zone_travail.py
+++ zone_travail.py
@@ -1,34 +1,69 @@
+#!/usr/bin/env python3
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
 from sympy import *
+from random import*
 
-#if __name__== '__main__':
-a = 45
-a= math.radians(a)
+# Fonctions
 
-Y1min = 0
-Y1max = 76
-Y1min = Y1min
-Y2max = Y1max
-pas = np.arange(0, Y1max + 0.1, 5)
+def Numero2(n):
+    L = [[None] * 2] * n
+    for k in range(n):
+        x, y = randint(50, 650), randint(50, 650)
+        L[k] = [x, y]
+    print(L)
+
+def constant (n, d, a):
+    I = [None] * n
+    for k in range(n):
+        I[k] = d
+        a.append(d)
+
+def croissante (a):
+    for n_i in deplacement_positif:
+        a.append(n_i)
+
+# def décroissante (a):
+#     for n_i in deplacement_positif:
 
 
 
+if __name__== '__main__':
+    a = 45
+    a= math.radians(a)
 
-cine_inverse = np.array([[tan(a), 1], [-tan(a), 1]])
+    Y1min = 0
+    Y1max = 76
+    Y1min = Y1min
+    Y2max = Y1max
+    pas = 1
+    deplacement_positif = np.arange(0, Y1max + 0.1, pas)
+    deplacement_negatif = np.arange(Y1max, 0 + 0.1, pas)
 
-Y1 = []
+    n = len(deplacement_positif)
 
-for n_i in pas:
-    Y1.append(pas)
+    print(n)
 
-pos_mot = np.array([Y1,[]])
-#
-# plaqueXY = np.array([[],[]])
-#
-# plaqueXY = cine_inverse.dot(pos_mot)
 
-print(pos_mot)
+    cine_inverse = np.array([[tan(a), 1], [-tan(a), 1]])
+
+    Y1 = []
+    Y2 = []
+
+    #constant(pas, 5, Y1)
+    #constant(pas, 0, Y1)
+    croissante(Y1)
+    print(Y1)
+    # Y1.reverse()
+    # print(Y1)
+
+    pos_mot = np.array([Y1, [Y2]])
+
+    plaqueXY = np.array([[],[]])
+    #
+    # plaqueXY = cine_inverse.dot(pos_mot)
+    #print(pos_mot)
